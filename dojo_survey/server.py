15956acde0ab78b3fcb72c8aefd3dbcdee19ff34
the-greenwell <ant.greenwell2@gmail.com>
2022-10-03 13:29:46 -0700

    Dojo survey, refactored

--- dojo_survey/server.py
+++ dojo_survey/server.py
@@ -4,20 +4,21 @@ from flask import Flask, render_template, redirect, request, url_for, session
 app = Flask(__name__)
 app.secret_key = 'soIgottaR1deWit0ne'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def root():
-    if request.method == 'POST':
-        session['form'] = request.form
-        session['food_pref'] = request.form.getlist('food_pref')
-        print(request.form)
-        return redirect('/result')
     return render_template("index.html")
 
+@app.route('/', methods=['POST'])
+def post_form():
+    session['form'] = request.form
+    session['food_pref'] = request.form.getlist('food_pref')
+    return redirect('/result')
+
 @app.route('/result')
-def handle_form():
+def render_form():
     if 'form' not in session:
         return redirect('/')
-    return render_template('user.html', form_data=session['form'], food_data=session['food_pref'])
+    return render_template('user.html')
 
 if __name__ == '__main__':
     app.run(host="localhost", port=3000, debug=True)
