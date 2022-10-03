983c722110912f19fb78470c9cfc535a45219b05
the-greenwell <ant.greenwell2@gmail.com>
2022-10-03 12:34:59 -0700

    counter project refactored

--- counter/server.py
+++ counter/server.py
@@ -1,22 +1,24 @@
+from curses.ascii import isdigit
 from flask import Flask, render_template, redirect, request, session
 
 app = Flask(__name__)
 app.secret_key = 'soIgottaR1deWit0ne'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def counter():
-    if request.method == 'POST':
-        session['increment'] = request.form['increment']
-
     if 'visits' in session:
         session['visits'] += 1
     else:
         session['visits'] = 1
-    times = 1
-    if 'increment' in session:
-        times = int(session['increment'])
-    print(times, session['visits'])
-    return render_template("index.html", visits=session['visits'], count=session['visits']*times, incremental=times)
+    if 'increment' not in session:
+        session['increment'] = 1
+    return render_template("index.html")
+
+@app.route('/', methods=['POST'])
+def post_increment():
+    if request.form['increment'].isdigit():
+        session['increment'] = int(request.form['increment'])
+    return redirect('/')
 
 @app.route('/destroy_session')
 def destroy_session():
